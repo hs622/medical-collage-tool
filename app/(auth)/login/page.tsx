85fe8f84fad60a63ca561dcdbe1ae80bb6652f01
Hussain Ali <hussainalee@icloud.com>
2026-04-09 20:02:50 +0500

    feat: add login page with theme toggle and login form

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"; 
+import { ThemeToggle } from "../../_components/theme-toggle";
+import LoginForm from "../../_components/forms/login-form";
+import { Suspense } from "react";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Welcome | Login",
+};
+
+export default function Page() {
+  return (
+    <div className="bg-muted">
+      <div className="p-4 float-end">
+        <ThemeToggle />
+      </div>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <Link href={"/"} className="flex items-center gap-2 self-center font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            </div>
+            aku.
+          </Link>
+          <Suspense>
+            <LoginForm />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  )
+}
