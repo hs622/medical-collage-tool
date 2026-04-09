51c7453d6e5a81bf94d38fe4bb6789c29d825a00
Hussain Ali <hussainalee@icloud.com>
2026-04-09 20:07:19 +0500

    feat: implement login form component with validation and loading state

---
+++ app/_components/forms/login-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+import { useForm } from "react-hook-form";
+import { Spinner } from "@/components/ui/spinner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TLoginSchema, LoginSchema } from '@/types/zod';
+import { CredentialSignIn } from "@/app/actions/auth-actions";
+
+export default function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+
+  const {
+    register,
+    // reset,
+    formState: { errors, isSubmitting },
+    handleSubmit
+  } = useForm<TLoginSchema>({
+    resolver: zodResolver(LoginSchema)
+  })
+
+  async function handleFormSubmit(data: TLoginSchema) {
+    await CredentialSignIn(data);
+
+    // console.log({status})
+    // console.log({url})
+    // console.log(code, ok, error)
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>
+            Login with your offical Email ID.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email@aku.edu"
+                  {...register("email")}
+                />
+                {errors.email && errors.email.message}
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                </div>
+                <Input id="password" type="password" {...register("password")} />
+                {errors.password && errors.password.message}
+              </Field>
+              <Field>
+                {isSubmitting ? (
+                  <Button type="submit" className="py-4" disabled>
+                    <Spinner />
+                    Login
+                  </Button>
+                ) : (
+                  <Button type="submit" className="py-4">Login</Button>
+                )}
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Link
+            href={"/forget-password"}
+            className="underline-offset-4 hover:underline"
+          >
+            Forgot your password?
+          </Link>
+        </CardFooter>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <Link href="#">Terms of Service</Link>{" "}
+        and <Link href="#">Privacy Policy</Link>.
+      </FieldDescription>
+    </div>
+  )
+}
