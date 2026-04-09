2c4991a9d52e1b873ee28db07cd4733972426ec4
Hussain Ali <hussainalee@icloud.com>
2026-04-09 20:11:53 +0500

    feat: add ForgetPasswordForm component for password recovery functionality

---
+++ app/_components/forms/forget-password-form.tsx
@@ -0,0 +1,65 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+export function ForgetPasswordForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Forget Password</CardTitle>
+          <CardDescription>
+            Enter your student Email ID.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email@aku.edu"
+                />
+              </Field> 
+              <Field>
+                <Button type="submit" className="py-4">Verify</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Link
+            href={"/"}
+            className="underline-offset-4 hover:underline"
+          >
+            back to Login
+          </Link>
+        </CardFooter>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  )
+}
