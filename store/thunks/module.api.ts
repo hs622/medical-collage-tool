e62034b1d15898be68f72c8288e3a7180ef3cb33
Hussain Ali <hussainalee@icloud.com>
2026-04-20 17:27:37 +0500

    feat: add module slice and fetchModuleData thunk for module state management

---
+++ store/thunks/module.api.ts
@@ -0,0 +1,19 @@
+// import { ApiResponseObject } from "@/types/api.zod";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchModuleData = createAsyncThunk(
+  "modules/fetchModules",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await fetch("/api/modules")
+      if (!res.ok) throw new Error("Failed to fetch.");
+      const parsed = await res.json()
+
+      if (parsed.status) return { 
+        ...parsed
+      };
+    } catch (err) {
+      return rejectWithValue(err || "something went wrong.")
+    }
+  },
+);
