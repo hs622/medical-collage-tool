cb77637a1266b1aac5da3ad302bbc94f2e90cd5d
Hussain Ali <hussainalee@icloud.com>
2026-04-20 17:44:25 +0500

    feat: add Notifications page with metadata and basic layout

---
+++ app/(dashboard)/d/notifications/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+  title: "Notifications",
+  description: ""
+}
+
+export default function Page() {
+
+  return (
+    <div className="">
+      notifications
+    </div>
+  )
+}
