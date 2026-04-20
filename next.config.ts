1fd65be56031ffbb9902ab2fcc25f9ff5f90d142
Hussain Ali <hussainalee@icloud.com>
2026-04-20 18:13:50 +0500

    fix: comment out unused turbopack configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
-import path from "path";
+// import path from "path";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.join(__dirname, ".."),
-  },
-  devIndicators: false
+  // turbopack: {
+  //   root: path.join(__dirname, ".."),
+  // },
+  // devIndicators: false
 };
 
 export default nextConfig;
