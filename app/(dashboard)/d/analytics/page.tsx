0d9161689291e55f300a48824ae48fe92ba671bb
Hussain Ali <hussainalee@icloud.com>
2026-04-20 17:46:30 +0500

    feat: add Analytics and HR Management pages with metadata and basic layout

---
+++ app/(dashboard)/d/analytics/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Analytics",
+  description: ""
+}
+
+export default function Page() {
+
+  return (
+    <div className="flex flex-col">
+      Analytics
+    </div>
+  )
+}
---
+++ app/(dashboard)/d/hr/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+  title: "HR Management",
+  description: ""
+}
+
+export default function Page() {
+
+  return (
+    <div>
+      hr management
+    </div>
+  )
+}
