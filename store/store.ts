348b19009a5b1b24f6bc6460a06d7ea6f8630685
Hussain Ali <hussainalee@icloud.com>
2026-04-20 18:18:33 +0500

    fix: remove unused slices from store configuration in store.ts

--- store/store.ts
+++ store/store.ts
@@ -1,14 +1,10 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import RBACSlice from './slices/rbacSlice';
-import StudentSlice from "./slices/studentSlice";
-import ModuleSlice from './slices/moduleSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import ModuleSlice from "./slices/moduleSlice";
 
 export const store = configureStore({
   reducer: combineReducers({
-    table: StudentSlice,
     module: ModuleSlice,
-    rbac: RBACSlice.reducer,
-  })
+  }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
