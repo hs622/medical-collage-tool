2b3b15cb413646b1a725510b5142e565e346ea4f
Hussain Ali <hussainalee@icloud.com>
2026-04-20 17:46:10 +0500

    feat: add Faculties and Students pages with metadata and basic layout

---
+++ app/(dashboard)/d/faculties/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+  title: "Faculties",
+  description: ""
+}
+
+export default function Page() {
+
+  return (
+    <div>
+      Faculties
+    </div>
+  )
+}
---
+++ app/(dashboard)/d/students/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+  title: "Students",
+  description: ""
+}
+
+export default function Page() {
+
+  return (
+    <div>
+      Students
+    </div>
+  )
+}
