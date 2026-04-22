4703ef338498006ea06362a32af986bb8baa27b9
Hussain Ali <hussainalee@icloud.com>
2026-04-22 15:43:21 +0500

    feat: add InitialState interface for data management in DataTable

--- app/_components/tables/common.d.ts
+++ app/_components/tables/common.d.ts
@@ -2,3 +2,13 @@
 export type DataTableProps<TData, TValue> = {
   columns: ColumnDef<TData, TValue>[]
 }
+
+export interface InitialState<Schema> {
+  currentPage: number;
+  limit: number;
+  skip: number;
+  total: number;
+  data: Schema[];
+  error: string | null;
+  status: "idle" | "loading" | "succeeded" | "failed";
+}
